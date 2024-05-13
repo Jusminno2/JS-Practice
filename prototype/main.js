e0d8eb36485cd2f32c9cee395dd00cd93f9c4c2c
GenUrakawa <g.urakawa@gmail.com>
2024-05-13 21:22:26 +0900

    for-in-loop だけが、prototype を表示する

--- prototype/main.js
+++ prototype/main.js
@@ -10,7 +10,22 @@ const obj = {
 
 // Object.create
 const ob = Object.create({ c: 333 });
+ob.a = 1;
+ob.b = 2;
 // Prototype を見るための関数
 console.log(Object.getPrototypeOf(obj));
 
 console.log(ob);
+
+// for in loop はプロトタイプまで見る => 例外 => 使う機会少ない
+for (const key in ob) {
+  console.log(key);
+}
+
+// Object.keys/values/entories はプロトタイプまでみない
+console.log(Object.keys(ob));
+
+// for of loop はプロトタイプをみない
+for (const key of Object.keys(ob)) {
+  console.log(key);
+}
