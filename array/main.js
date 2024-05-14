8789a8e60bce10f354b8a3edd1aaabf06d9173af
GenUrakawa <g.urakawa@gmail.com>
2024-05-15 08:23:28 +0900

    スプレッド構文を追加

--- array/main.js
+++ array/main.js
@@ -9,3 +9,17 @@ let fruits = ['apple', 'orange', 'banana'];
 // fruits = new Array('apple', 'orange', 'banana')
 fruits[3] = 'grape';
 console.log(fruits);
+
+/*-----------------------スプレット構文---------------------------------------------------------*/
+fruits = ['apple', 'orange', 'banana'];
+const newFruits = ['peach', ...fruits, 'mango'];
+fruits.push('grape');
+let sum = (...nums) => {
+  console.log(nums);
+};
+let nums = [1, 2, 3, 4];
+sum(...nums); //しっかりと配列として返る
+/*
+  もし、sum(nums)だったら、[[1,2,3,4]]となっちゃう
+  ⇒スプレット構文はこれを解決
+*/
