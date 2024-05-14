3c5cc65517bd9a07528bdd561997652b2014ef8c
GenUrakawa <g.urakawa@gmail.com>
2024-05-14 09:39:14 +0900

    関数はprototypeを持ち、それはObjectのPrototypeとはまた別物

--- prototype/main.js
+++ prototype/main.js
@@ -39,7 +39,7 @@ const UserFactory = (name, age) => {
   };
 };
 // const user1 = UserFactory('Yoshipi', 30);
-// const user2 = UserFactory('Yoshiki', 20);
+// concar = UserFactory('Yoshiki', 20);
 // const user3 = UserFactory('Konno', 81);
 
 // インスタンス生成の知られざる裏側
@@ -55,3 +55,26 @@ const user1 = new UserConstructor('Yoshipi', 30);
 const user2 = new UserConstructor('Yoshiki', 20);
 const user3 = new UserConstructor('Konno', 81);
 console.log(user1, user2, user3);
+
+/* 
+    インスタンス生成の知られざる裏側２
+    ・すべての関数（arrow関数を除く）は、prototypeを持っている
+    ・オブジェクトの中に入っているprototypeとは別もの
+    ・new演算子によってコンストラクタ関数が作られたときに、使われるprototype
+    ・メモリの節約になる
+*/
+const CarConstructor = function (name, price) {
+  // new で呼び出す=>ここで暗黙的にthisが"create"で作られる. this.~はオブジェクトに要素を追加している
+  //this = Object.create(CarConstructor.prototype)
+  this.name = name;
+  this.price = price;
+  // this.greeting = function () {};
+  //return this;
+};
+CarConstructor.prototype.greeting = function () {
+  return `Hi! This car's name is ${this.name}. This costs you ${this.price} to buy.`;
+};
+const car1 = new CarConstructor('Toyota', 300000);
+const car2 = new CarConstructor('Tesla', 20000);
+const car3 = new CarConstructor('Honda', 81000);
+console.log(car1, car2, car3);
