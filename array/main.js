72cf3780a2f59699ce3f1824b56ae559bb8d8850
GenUrakawa <g.urakawa@gmail.com>
2024-05-16 16:57:42 +0900

    splice/copyWithin/reverse/sort/slice/concat/join/indexOf/includes

--- array/main.js
+++ array/main.js
@@ -63,3 +63,48 @@ console.log(arrayLikeObject);
 //厳密にはarrayLikeObjectを参考にして、新たな配列を作る
 const realArray = Array.from(arrayLikeObject);
 console.log(realArray, arrayLikeObject);
+
+/*------------------------- 配列自体を変えるメソッド ----------------------------------------------*/
+items = [0, 1, 2];
+// splice(削除始めたいindex, 削除する数, 削除したものと置き換えるもの)
+items.splice(0, 1, 22);
+console.log(items);
+
+items = [0, 1, 2, 3];
+// copyWithin(入れ替えたい場所, コピーの始まり, コピーの終わり)
+items.copyWithin(0, 2, 4);
+console.log(items); //[2,3,2,3,]
+
+items = [0, 1, 2, 3];
+// 逆転
+items.reverse();
+console.log(items);
+
+items = [0, 1, undefined, 2, 3, undefined];
+// 文字列に変換して辞書順に並べ替える
+items.sort();
+console.log(items);
+
+/*--------------------------配列は変えないメソッド ----------------------------------------------------*/
+items = [0, 1, 2, 3, 4];
+// itemsには、変更を加えずに返り値だけを取るメソッド
+let result = items.slice(2, 4); //[2,3]
+console.log(result);
+
+items = [0, 1, 2, 3, 4, 5];
+result = items.concat([6, 7, 8]);
+console.log(result);
+
+items = [0, 1, 2, 3, 4, 5];
+result = items.join('');
+console.log(result);
+
+items = ['banana', 'apple', 'orange', 'banana'];
+// 第一引数：探したい文字列
+// 第二引数：探すのスタートしたい場所
+// 返り値：その文字列があるindex番号 => 何もなかった場合は-1が返る
+result = items.indexOf('banana', 2);
+// 返り値=>あったらTrue, なかったらFalse
+result_2 = items.includes('banana', 2);
+console.log(result);
+console.log(result_2);
