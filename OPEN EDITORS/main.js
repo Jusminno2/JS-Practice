e65054cf22cce781bf7d5e92179886941a56945e
GenUrakawa <g.urakawa@gmail.com>
2024-05-16 19:48:55 +0900

    setTimeout について

--- OPEN EDITORS/main.js
+++ OPEN EDITORS/main.js
@@ -1,9 +1,9 @@
-let result = confirm('Are you sure you?');
-console.log(result); // OKやったらtrueを返す
+// let result = confirm('Are you sure you?');
+// console.log(result); // OKやったらtrueを返す
 
 // 入力できるよ
-result = prompt('name', 'ug');
-console.log(result); //ug
+// result = prompt('name', 'ug');
+// console.log(result); //ug
 
 // 指定したurlを解析したものをオブジェクトとして展開している
 let url = new URL('https://sports.yahoo.co.jp/');
@@ -21,3 +21,30 @@ port: ""
 protocol: "https:"
 search: ""
 --------------------------------------------------------------------------------------------------------------*/
+
+// 少なくとも2000msは待つという意味
+setTimeout(() => {
+  console.log('hello world!!');
+}, 2000);
+console.log('hello super world!!'); // 先に実行
+
+// setTImeoutが0sのときでも、その後の処理が先に実行される
+setTimeout(() => {
+  console.log('hello world!!');
+}, 0);
+console.log('hello super world!!'); // 先に実行
+console.log('apple');
+/*
+  hello super world!!
+  apple
+  hello world!!
+*/
+
+// 一定間隔で処理を実行したいとき
+let timerId = setInterval(() => {
+  console.log('hello super apple!!');
+}, 1000);
+// 処理をある時やめたいとき
+setTimeout(() => {
+  clearInterval(timerId);
+}, 5000);
