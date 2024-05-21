1cb63b04c13058043b42c875324d3649369018eb
GenUrakawa <g.urakawa@gmail.com>
2024-05-21 23:30:07 +0900

    プロミスチェーン

--- async/main.js
+++ async/main.js
@@ -28,6 +28,7 @@
 
     **C++のエラー（API）はトライキャッチできない**
 */
+/*
 window.addEventListener('click', e => {
   console.log(e);
   setTimeout(() => {
@@ -40,6 +41,7 @@ window.addEventListener('click', e => {
     });
   }, 1000);
 });
+*/
 
 /*
   Promise
@@ -48,14 +50,71 @@ window.addEventListener('click', e => {
   ・第二引数に、rejectオブジェクトを持つ
   ・promiseは、オブジェクト[PromiseState],[promiseResult]を返す
 */
+/*
 let promise = new Promise((resolve, reject) => {
   resolve('hello');
   console.log('hello promise');
 });
 console.log(promise);
+*/
 
 /*
   [[Prototype]]: Promise
   [[PromiseState]]: "fulfilled"
   [[PromiseResult]]: "hello"
 */
+
+// promiseがresolveされるまでは、終わらねえって話
+/*
+promise = new Promise(resolve => {
+  let tmpPromise = new Promise(resolve2 => {
+    setTimeout(() => {
+      resolve2();
+    }, 1000);
+  });
+  resolve(tmpPromise);
+});
+*/
+
+/*
+  promise チェーンでできること
+  １．thenは何回でも繰り返して使える
+  ２．catchのあとにthenが使える
+  ３．then or catch で return されたあとは、thenを探し始める
+  ４．throw では catch 探し
+  ５．finallyはじゅんばんが来たら必ず実行
+  ６．そのあとthen探しを再開する
+  ７．then探し中にfinallyでthrowされたらキャッチ探しが始まる
+*/
+new Promise(resolve => resolve(1))
+  .then(value => {
+    console.log(value);
+    return 2;
+  })
+  .then(value => {
+    console.log(value);
+    throw new Error(3);
+  })
+  .catch(error => {
+    console.log(error.message);
+    throw new Error(4);
+  })
+  .catch(error => {
+    console.log(error.message);
+    return 5;
+  })
+  .then(value => {
+    console.log(value);
+    return 6;
+  })
+  // then or catch で return されたあとは、thenを探し始める
+  .catch(() => console.log('skip'))
+  .catch(() => console.log('skip'))
+  .then(value => {
+    console.log(value);
+    throw new Error(7);
+  })
+  .then(() => console.log('skip'))
+  .then(() => console.log('skip'))
+  // throwはcatchで
+  .catch(error => console.log(error.message));
